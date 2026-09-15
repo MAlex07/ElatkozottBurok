606c20a6e0c767d9ddc93dc31fd085335385fce7
MAlex07 <molnar.alex@petrik.hu>
2026-09-15 11:16:38 +0200

    automata elkezdve

--- ElatkozottBurok/Automata.cs
+++ ElatkozottBurok/Automata.cs
@@ -6,7 +6,36 @@ namespace ElatkozottBurok
 {
     public class Automata
     {
+        private int keszpenzKassza;
+        private List<Nassolnivalo> keszlet;
+        private bool elakadva;
+
+        public Automata(int keszpenzKassza, List<Nassolnivalo> készlet, bool elakadva)
+        {
+            this.keszpenzKassza = keszpenzKassza;
+            this.keszlet = készlet;
+            this.elakadva = elakadva;
+        }
+
+        public int KeszpenzKassza { get => keszpenzKassza; set => keszpenzKassza = value; }
+        public List<Nassolnivalo> Keszlet { get => keszlet; set => keszlet = value; }
+        public bool Elakadva { get => elakadva; set => elakadva = value; }
+
+
+        private void Feltolt(List<Nassolnivalo> nassolnivalos)
+        {
+            Keszlet.AddRange(nassolnivalos);
+        }
+
+        //private Nassolnivalo Vasarlas(string termekNev, Fejleszto vasarlo)
+        //{
+        //    if (elakadva)
+        //    {
+        //        Fejleszto fejleszto.stresszSzint += 15;
+        //    }
+        //}
         
+
         
     }
 }
